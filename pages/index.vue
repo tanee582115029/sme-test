9e3e7913e8c2271fe2103eec4d34e507eece8435
Tanee <tanee_yooyen@cmu.ac.th>
2020-06-04 14:58:49 +0700

    ok

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
+<template>
+    <div>
+        hi
+    </div>
+</template>
 <script>
 import base64 from 'base-64'
 import md5 from 'md5'
@@ -9,30 +14,6 @@ export default {
             action: "",
             params: "",
         }
-    },
-    mounted() {
-        // 1654109263-01oqemwP
-        liff.init({ liffId: "1654109263-01oqemwP" }, () => {
-            const queryString = decodeURIComponent(window.location.search).replace("?liff.state=", "")
-            const params = new URLSearchParams(queryString)
-            this.params = params
-            this.action = params.get('action')
-            if (liff.isLoggedIn()) {
-                liff.getProfile().then(profile => {
-                    const today = moment().format('YYYY MM DD')
-                    const encode = base64.encode(md5(today+"A578RT"))
-                    const userId = profile.userId
-                    if (this.action === 'register') {
-                        window.location.href = `https://backend.halalhero.co/register/?line=${userId}&key=${encode}`
-                    }
-                    else {
-                        window.location.href = `https://hlc.avalue.co.th/restaurant?userId=${userId}`
-                    }
-                }).catch(err => console.log(err))
-            } else {
-                liff.login()
-            }
-        }, err => console.error(err.code, error.message))
     },
     head() {
         return {
