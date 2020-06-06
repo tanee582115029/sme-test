cc9f17ac76e7a92fc39f83c0a8f27bbfb6c450d9
Tanee <tanee_yooyen@cmu.ac.th>
2020-06-06 14:34:58 +0700

    change to new path

--- pages/index.vue
+++ pages/index.vue
@@ -1,88 +1,42 @@
-<template>
-    <div>
-            <div class="form-text" @submit.prevent="addData">
-                Name: <input v-model="name" /> <br/>
-                Province: <input v-model="province" /><br/>
-                <button color="success" class="mr-4" @click="addData">Submit</button><br/>
-                <a href="https://sme-test.herokuapp.com/edit" type="button">edit</a>
-            </div>
-        <div class="content"
-            v-for="(user, key) in userList"
-            :key=key outlined tile
-        >
-            {{'Name:'+ user.name }} <br/><br/>
-            {{'Province:'+ user.province }} <br/><br/>
-            {{'UserId:'+ user.userId }}<br/><br/>
-            {{'TimeStamp:'+ user.timestamp }}
-        </div>
-    </div>
-</template>
-
 <script>
-import { db } from '~/firebaseConfig.js'
-import firebase from 'firebase'
+import base64 from 'base-64'
+import md5 from 'md5'
+import moment from 'moment'
 export default {
-    data: function () {
+    layout: 'mobile',
+    data() {
         return {
-            name: null,
-            province: null,
-            userList: [],
-            userId: null,
+            action: "",
+            params: "",
         }
     },
-    methods: {
-        addData() {
-            liff.init({ liffId: "1654304970-y8m7gggm" }, () => {
-                if (liff.isLoggedIn()) {
-                    liff.getProfile().then(profile => {
-                        const data = {
-                            name: this.name,
-                            userId: profile.userId,
-                            province: this.province,
-                            timestamp: firebase.firestore.FieldValue.serverTimestamp()
-                        }
-                        db.collection("user").doc(profile.userId).set(data)
-                            .then(function() {
-                                console.log("Document successfully written!")
-                            })
-                            .catch(function(error) {
-                                console.error("Error writing document: ", error)
-                            })
-                    }).catch(err => console.log(err))
-                } else {
-                    liff.login()
-                }
-            }, err => console.error(err.code, error.message))
-        },
-        getData() {
-            db.collection("user")
-                .onSnapshot((querySnapshot) => {
-                    var data = []
-                    querySnapshot.forEach((doc) => {
-                        data.push(doc.data())
-                    })
-                    this.userList = data
-                }).then(()=>{
-                })
-        },
-    },
     mounted() {
-        this.getData()
+        liff.init({ liffId: "1654304970-y8m7gggm" }, () => {
+            const queryString = decodeURIComponent(window.location.search).replace("?liff.state=", "")
+            const params = new URLSearchParams(queryString)
+            this.params = params
+            this.action = params.get('action')
+            if (liff.isLoggedIn()) {
+                liff.getProfile().then(profile => {
+                    // const today = moment().format('YYYY MM DD')
+                    // const encode = base64.encode(md5(today+"A578RT"))
+                    // const userId = profile.userId
+                    // window.location.href = `https://backend.halalhero.co/register/?line=${userId}&key=${encode}`
+                    window.location.href = `http://reg.advicesys.com/`
+                }).catch(err => console.log(err))
+            } else {
+                liff.login()
+            }
+        }, err => console.error(err.code, error.message))
     },
-    created() {
-        this.getData()
+    head() {
+        return {
+            title: 'Register',
+        }
     }
 }
 </script>
 
-<style>
-.content {
-    margin-top: 40px;
-    display: flex;
-    justify-content: center;
-}
-.form-text {
-    margin-top: 40px;
-    margin-left: 80px;
-}
+<style lang="scss" scoped>
+    //
 </style>
