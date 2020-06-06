37b860389cba9f0410edac273ba7a0e85343ff56
Tanee <tanee_yooyen@cmu.ac.th>
2020-06-06 16:25:33 +0700

    remove flex

--- pages/searchProduct.vue
+++ pages/searchProduct.vue
@@ -13,38 +13,9 @@ export default {
     mounted() {
         liff.init({ liffId: "1654312520-x4ByNM87" }, () => {
             if (liff.isLoggedIn()) {
-                // window.location.href = `http://reg.advicesys.com/group/`
+                window.location.href = `http://reg.advicesys.com/group/`
                 // liff.getProfile().then(profile => {
                 // }).catch(err => console.log(err))
-                if (!liff.isInClient()) {
-                    sendAlertIfNotInClient()
-                }
-                else {
-                    liff.sendMessages([ {
-                        "type": "flex",
-                        "altText": "Flex Message",
-                        "contents": {
-                            "type": "bubble",
-                            "direction": "ltr",
-                            "hero": {
-                                "type": "image",
-                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_4_news.png",
-                                "size": "full",
-                                "aspectRatio": "20:13",
-                                "aspectMode": "cover",
-                                "action": {
-                                    "type": "uri",
-                                    "label": "Action",
-                                    "uri": "https://linecorp.com/"
-                                }
-                            }
-                        }
-                    } ]).then(function() {
-                        window.alert('Message sent')
-                    }).catch(function(error) {
-                        window.alert('Error sending message: ' + error)
-                    })
-                }
             }
             else {
                 liff.login()
