f7b0c1a2284eb55ad51bd7dddd2b4f444dec5d98
Tanee <tanee_yooyen@cmu.ac.th>
2020-06-06 15:56:29 +0700

    add flex msg in liff function

--- pages/searchProduct.vue
+++ pages/searchProduct.vue
@@ -13,10 +13,40 @@ export default {
     mounted() {
         liff.init({ liffId: "1654312520-x4ByNM87" }, () => {
             if (liff.isLoggedIn()) {
-                window.location.href = `http://reg.advicesys.com/group/`
+                // window.location.href = `http://reg.advicesys.com/group/`
                 // liff.getProfile().then(profile => {
                 // }).catch(err => console.log(err))
-            } else {
+                if (!liff.isInClient()) {
+                    sendAlertIfNotInClient()
+                }
+                else {
+                    liff.sendMessages([ {
+                        "type": "flex",
+                        "altText": "Flex Message",
+                        "contents": {
+                            "type": "bubble",
+                            "direction": "ltr",
+                            "hero": {
+                                "type": "image",
+                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_4_news.png",
+                                "size": "full",
+                                "aspectRatio": "20:13",
+                                "aspectMode": "cover",
+                                "action": {
+                                    "type": "uri",
+                                    "label": "Action",
+                                    "uri": "https://linecorp.com/"
+                                }
+                            }
+                        }
+                    } ]).then(function() {
+                        window.alert('Message sent')
+                    }).catch(function(error) {
+                        window.alert('Error sending message: ' + error)
+                    })
+                }
+            }
+            else {
                 liff.login()
             }
         }, err => console.error(err.code, error.message))
