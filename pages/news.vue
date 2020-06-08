c6dc158159d8caefd9822d945eaaa0e1d082e7c1
Tanee <tanee_yooyen@cmu.ac.th>
2020-06-08 13:30:09 +0700

    add news

---
+++ pages/news.vue
@@ -0,0 +1,33 @@
+<script>
+import base64 from 'base-64'
+import md5 from 'md5'
+import moment from 'moment'
+export default {
+    layout: 'mobile',
+    data() {
+        return {
+            action: "",
+            params: "",
+        }
+    },
+    mounted() {
+        liff.init({ liffId: "1654312520-0dkM8qQj" }, () => {
+            if (liff.isLoggedIn()) {
+                window.location.href = `http://reg.advicesys.com/news/`
+            }
+            else {
+                liff.login()
+            }
+        }, err => console.error(err.code, error.message))
+    },
+    head() {
+        return {
+            title: 'ข่าว',
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    //
+</style>
