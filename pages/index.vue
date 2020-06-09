c9259cd52f76e15dcbd5349c53d4c2d922a70a30
Tanee <tanee_yooyen@cmu.ac.th>
2020-06-09 17:58:53 +0700

    update get categories list

--- pages/index.vue
+++ pages/index.vue
@@ -12,16 +12,21 @@ export default {
     },
     mounted() {
         liff.init({ liffId: "1654312520-GlMnRVqX" }, () => {
-            // const queryString = decodeURIComponent(window.location.search).replace("?liff.state=", "")
-            // const params = new URLSearchParams(queryString)
-            // this.params = params
-            // this.action = params.get('action')
+            const queryString = decodeURIComponent(window.location.search).replace("?liff.state=", "")
+            const params = new URLSearchParams(queryString)
+            this.params = params
+            this.action = params.get('GID')
             if (liff.isLoggedIn()) {
                 liff.getProfile().then(profile => {
                     const today = moment().format('YYYY MM DD')
                     const encode = base64.encode(md5(today+"A578RT"))
                     const userId = profile.userId
-                    window.location.href = `http://reg.advicesys.com/register/?line=${userId}&key=${encode}`
+                    if (this.action === '2') {
+                        window.location.href = `http://reg.advicesys.com/lists/?GID=2`
+                    }
+                    else {
+                        window.location.href = `http://reg.advicesys.com/register/?line=${userId}&key=${encode}`
+                    }
                 }).catch(err => console.log(err))
             } else {
                 liff.login()
