19f46932de5de2a57fb1962a1e9188e03c0e1d98
Tanee <tanee_yooyen@cmu.ac.th>
2020-06-10 14:55:50 +0700

    update loading

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
 <div>
-        <div v-if="isLoading">
+        <div
+            v-if="isLoading"
+            class="loading-wrap"
+        >
             <a-icon
                 type="loading"
-                style="color: #FF9D1A; fontSize: 50px;"
+                style="color: #014D87; fontSize: 50px;"
                 theme="outlined"
             />
         </div>
-        <div v-else-if="!isLoading">
-            <a :href="`https://community.smeconnext.com/lists/?GID=${action}`">next</a>
+        <div class="btn-wrap" v-else-if="!isLoading">
+            <a-button class="next-btn" :href="`https://community.smeconnext.com/lists/?GID=${action}`">ดำเนินการต่อ</a-button>
         </div>
 </div>
 </template>
@@ -16,9 +19,13 @@
 import base64 from 'base-64'
 import md5 from 'md5'
 import moment from 'moment'
-import { Icon } from 'ant-design-vue'
+import { Icon, Button } from 'ant-design-vue'
 export default {
     layout: 'mobile',
+    components: {
+        AIcon: Icon,
+        AButton: Button
+    },
     data() {
         return {
             action: "",
@@ -30,7 +37,7 @@ export default {
     mounted() {
         setTimeout(() => {
             this.isLoading = false
-        }, 5000)
+        }, 3000)
         liff.init({ liffId: "1654312520-GlMnRVqX" }, () => {
             const queryString = decodeURIComponent(window.location.search).replace("?liff.state=", "")
             const params = new URLSearchParams(queryString)
@@ -98,7 +105,33 @@ export default {
     }
 }
 </script>
-
-<style lang="scss" scoped>
-    //
+<style>
+.loading-wrap {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background-color: rgb(255, 255, 255);
+    z-index: 2000;
+}
+.btn-wrap {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    z-index: 2000;
+}
+.next-btn {
+    font-size: 16px;
+}
 </style>
--- pages/myaccount.vue
+++ pages/myaccount.vue
@@ -1,22 +1,48 @@
+<template>
+<div>
+        <div
+            v-if="isLoading"
+            class="loading-wrap"
+        >
+            <a-icon
+                type="loading"
+                style="color: #014D87; fontSize: 50px;"
+                theme="outlined"
+            />
+        </div>
+        <div class="btn-wrap" v-else-if="!isLoading">
+            <a-button class="next-btn" :href="`https://community.smeconnext.com/profile/?line=${userId}&key=${encode}`">ดำเนินการต่อ</a-button>
+        </div>
+</div>
+</template>
 <script>
 import base64 from 'base-64'
 import md5 from 'md5'
 import moment from 'moment'
+import { Icon, Button } from 'ant-design-vue'
 export default {
     layout: 'mobile',
     data() {
         return {
             action: "",
             params: "",
+            isLoading: true,
+            userId: "",
+            encode: "",
         }
     },
     mounted() {
+        setTimeout(() => {
+            this.isLoading = false
+        }, 3000)
         liff.init({ liffId: "1654312520-gER41Pep" }, () => {
             if (liff.isLoggedIn()) {
                 liff.getProfile().then(profile => {
                     const today = moment().format('YYYY MM DD')
                     const encode = base64.encode(md5(today+"A578RT"))
                     const userId = profile.userId
+                    this.userId = userId
+                    this.encode = encode
                     window.location.href = `https://community.smeconnext.com/profile/?line=${userId}&key=${encode}`
                 }).catch(err => console.log(err))
             } else {
