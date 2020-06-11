9e5df30672d473c0e340d5b88b129dd8d2f99d66
Tanee <tanee_yooyen@cmu.ac.th>
2020-06-11 10:39:14 +0700

    update wrong path

--- pages/categories.vue
+++ pages/categories.vue
@@ -54,35 +54,35 @@ export default {
                     this.encode = encode
                     if (this.action === '93') {
                         this.title = 'ข้าว'
-                        window.location.href = `https://community.smeconnext.com/lists/?GID=2&line=${userId}&key=${encode}`
+                        window.location.href = `https://community.smeconnext.com/lists/?GID=93&line=${userId}&key=${encode}`
                     }
                     else if (this.action === '94') {
                         this.title = 'น้ำดื่ม'
-                        window.location.href = `https://community.smeconnext.com/lists/?GID=3&line=${userId}&key=${encode}`
+                        window.location.href = `https://community.smeconnext.com/lists/?GID=94&line=${userId}&key=${encode}`
                     }
                     else if (this.action === '95') {
                         this.title = 'ผ้า/เสื้อผ้า'
-                        window.location.href = `https://community.smeconnext.com/lists/?GID=4&line=${userId}&key=${encode}`
+                        window.location.href = `https://community.smeconnext.com/lists/?GID=95&line=${userId}&key=${encode}`
                     }
                     else if (this.action === '96') {
                         this.title = 'ผลไม้/อาหารแปรรูป'
-                        window.location.href = `https://community.smeconnext.com/lists/?GID=5&line=${userId}&key=${encode}`
+                        window.location.href = `https://community.smeconnext.com/lists/?GID=96&line=${userId}&key=${encode}`
                     }
                     else if (this.action === '97') {
                         this.title = 'ขนมขบเคี้ยว'
-                        window.location.href = `https://community.smeconnext.com/lists/?GID=6&line=${userId}&key=${encode}`
+                        window.location.href = `https://community.smeconnext.com/lists/?GID=97&line=${userId}&key=${encode}`
                     }
                     else if (this.action === '98') {
                         this.title = 'ผลิตภัณฑ์เพื่อสุขภาพ'
-                        window.location.href = `https://community.smeconnext.com/lists/?GID=7&line=${userId}&key=${encode}`
+                        window.location.href = `https://community.smeconnext.com/lists/?GID=98&line=${userId}&key=${encode}`
                     }
                     else if (this.action === '99') {
                         this.title = 'ของใช้/ของตกแต่ง'
-                        window.location.href = `https://community.smeconnext.com/lists/?GID=8&line=${userId}&key=${encode}`
+                        window.location.href = `https://community.smeconnext.com/lists/?GID=99&line=${userId}&key=${encode}`
                     }
                     else {
                         this.title = 'ผลิตภัณฑ์บำรุงดิน'
-                        window.location.href = `https://community.smeconnext.com/lists/?GID=9&line=${userId}&key=${encode}`
+                        window.location.href = `https://community.smeconnext.com/lists/?GID=100&line=${userId}&key=${encode}`
                     }
                 }).catch(err => console.log(err))
             } else {
